1ce90c2d02acb62b035f52ca8f50b81cbb2e6648
dgstjohn <dgstjohn@gmail.com>
2021-06-22 20:59:53 -0400

    added task deletion functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 var taskIdCounter = 0;
+var pageContentEl = document.querySelector("#page-content");
 
 var taskFormHandler = function (event) {
   event.preventDefault();
@@ -43,7 +44,7 @@ var createTaskEl = function (taskDataObj) {
 
   var taskActionsEl = createTaskActions(taskIdCounter);
   listItemEl.appendChild(taskActionsEl);
-  
+
   // add entire list item to list
   tasksToDoEl.appendChild(listItemEl);
 
@@ -93,4 +94,19 @@ var createTaskActions = function (taskId) {
   return actionContainerEl;
 };
 
+var deleteTask = function (taskId) {
+  var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+  taskSelected.remove();
+  console.log(taskSelected);
+};
+
+var taskButtonHandler = function (event) {
+  console.log(event.target);
+  if (event.target.matches(".delete-btn")) {
+    var taskId = event.target.getAttribute("data-task-id");
+    deleteTask(taskId);
+  }
+};
+
 formEl.addEventListener("submit", taskFormHandler);
+pageContentEl.addEventListener("click", taskButtonHandler);
